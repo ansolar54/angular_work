3d95b929258c0e2d6b0d6a426acbf13363ae948f
ansolar54 <ansolar54@gmail.com>
2021-09-07 16:20:05 -0500

    avance 7/09 afternoon

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -2,26 +2,29 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
 
+import { DbzService } from '../services/dbz.service';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
-   personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ]
+  nuevo: Personaje = {
+    nombre: "Maestro Rochi",
+    poder: 1000
+  }
 
-  nuevo: Personaje = 
-    {
- nombre: "Maestro Rochi",
- poder: 1000
-    }
+  // get personajes(): Personaje[]{
+  //   return this.dbzService.personajes;
+  // }
+
+  // agregarNuevoPersonaje(argumento: Personaje) {
+  //   this.personajes.push(argumento);
+  //   // console.log(argumento);
+  // }
+  
+  constructor(){}
+
+
 }
